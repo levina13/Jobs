f1584f39f16ab8955194c303e239d4968216c1cd
levina13 <87984577+levina13@users.noreply.github.com>
2023-03-14 11:02:52 +0700

    tambah map um

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -306,7 +306,7 @@
               </div>
 
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621"
+                src="https://www.google.com/maps/embed/v1/place?q=Universitas+Negeri+Malang,+Jalan+Ambarawa,+Sumbersari,+Malang+City,+East+Java,+Indonesia&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
                 frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
             </div>
 
