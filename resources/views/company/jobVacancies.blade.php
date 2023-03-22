c845649cf6185737276e31922e625d391c0b7df6
levina13 <87984577+levina13@users.noreply.github.com>
2023-03-22 20:46:49 +0700

    applicant & jobVacancies views lintang

--- resources/views/company/jobVacancies.blade.php
+++ resources/views/company/jobVacancies.blade.php
@@ -6,8 +6,84 @@
 <!-- partial -->
 <div class="main-panel">
     <div class="content-wrapper">
-
+        <div>
+            <a href="{{route('view.company.jobVacancies.create')}}">
+                <button type="button" class="btn btn-gradient-info btn-lg btn-block">Add jobs vacancies</button>
+            </a>
+        </div>
+        <br>
+        <br>
+        <br>
+        <br>
+        <table class="table table-striped">
+            <thead>
+              <tr>
+                <th> company logo </th>
+                <th> Position </th>
+                <th> Company name </th>
+                <th> Company address </th>
+                <th>  </th>
+              </tr>
+            </thead>
+            <tbody>
+                <tr>
+                  <td class="py-1">
+                    <img src="../../assets/images/faces-clipart/pic-1.png" alt="image" />
+                  </td>
+                  <td> Sales Marketing </td>
+                  <td> Alibaba </td>
+                  <td> malang </td>
+                  <td>
+                    <button type="button" class="btn btn-gradient-primary btn-rounded btn-sm">Show</button>
+                    <button type="button" class="btn btn-gradient-info btn-rounded btn-sm">Edit</button>
+                    <button type="button" class="btn btn-gradient-danger btn-rounded btn-sm">Delete</button>
+                  </td>
+                </tr>
+                <tr>
+                    <td class="py-1">
+                      <img src="../../assets/images/faces-clipart/pic-2.png" alt="image" />
+                    </td>
+                    <td> Admin </td>
+                    <td> Alibaba </td>
+                    <td> Malang </td>
+                    <td>
+                        <button type="button" class="btn btn-gradient-primary btn-rounded btn-sm">Show</button>
+                      <button type="button" class="btn btn-gradient-info btn-rounded btn-sm">Edit</button>
+                      <button type="button" class="btn btn-gradient-danger btn-rounded btn-sm">Delete</button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="py-1">
+                      <img src="../../assets/images/faces-clipart/pic-1.png" alt="image" />
+                    </td>
+                    <td> Staf </td>
+                    <td> Alibaba </td>
+                    <td> malang </td>
+                    <td>
+                      <button type="button" class="btn btn-gradient-primary btn-rounded btn-sm">Show</button>
+                      <button type="button" class="btn btn-gradient-info btn-rounded btn-sm">Edit</button>
+                      <button type="button" class="btn btn-gradient-danger btn-rounded btn-sm">Delete</button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="py-1">
+                      <img src="../../assets/images/faces-clipart/pic-2.png" alt="image" />
+                    </td>
+                    <td> Manager </td>
+                    <td> Alibaba </td>
+                    <td> Malang </td>
+                    <td>
+                        <button type="button" class="btn btn-gradient-primary btn-rounded btn-sm">Show</button>
+                      <button type="button" class="btn btn-gradient-info btn-rounded btn-sm">Edit</button>
+                      <button type="button" class="btn btn-gradient-danger btn-rounded btn-sm">Delete</button>
+                    </td>
+                  </tr>
+            </tbody>
+        </table>
     </div>
+
+
+
           <!-- content-wrapper ends -->
 
 @endsection
