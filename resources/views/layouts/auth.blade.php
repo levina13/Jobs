da306bd6aa00b798481bd75cdb86b98884e1640e
Dinda Rahma Juwita <dindrahma706@gmail.com>
2023-03-23 09:46:26 +0700

    shortcut icon

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -8,6 +8,7 @@
   <link href="{{asset('assets/vendor/bootstrap/css/bootstrap.css') }}" rel="stylesheet">
   <link href="{{asset('css/logRegStyle.css') }}" rel="stylesheet">
   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+  <link rel="shortcut icon" href="{{asset('assets/img/jobsicon.png')}}"/>
   <script src="{{asset('js/jquery-3.6.3.min.js')}}"></script>
   <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
