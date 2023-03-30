aeaa675d6452804583e4884c8b70e9a12824ad84
levina13 <87984577+levina13@users.noreply.github.com>
2023-03-30 18:37:46 +0700

    mau merge

--- app/Http/Controllers/applicant/UsersController.php
+++ app/Http/Controllers/applicant/UsersController.php
@@ -5,10 +5,11 @@ namespace App\Http\Controllers\applicant;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
-    function showProfile($id){
+    function showProfile(){
         $data = User::select(
             'users.*',
             'cities.city',
@@ -17,13 +18,13 @@ class UsersController extends Controller
             ->leftJoin('cities','cities.id','=','users.id_city')
             ->leftJoin('provinces','provinces.id','=','cities.id_province')
             ->leftJoin('education','education.id','=','users.id_education')
-            ->where('users.id','=',$id)
+            ->where('users.id','=',Auth::user()->id)
             ->first();
 
         return $data;
     }
     function viewEditProfile($id){
-        $
+        
     }
     function editProfile($id){
 
