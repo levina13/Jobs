53ab7aad43893f6ab62617f2a6c7532bc6f8dc82
levina13 <87984577+levina13@users.noreply.github.com>
2023-04-03 21:47:03 +0700

    html->view belum database template cv

---
+++ resources/views/cv/template/creative3.blade.php
@@ -0,0 +1,77 @@
+<!Doctype HTML>
+<html>
+<head>
+	<title>Creative 3</title>
+	<link rel="stylesheet" type="text/css" href="css/creative3.css"/>
+</head>
+
+<body>
+	<div class="box-outer">
+	<div class="resume-box">
+		<div class="box-1">
+			<img src="images/profile.png" class="profile" />
+			<div class="content">
+			<h1>About me</h1>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+				<br/><br/>
+				Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+			</p>
+
+			<h1>Skills</h1>
+			<p>Grapic Design</p>
+			<div id="progress"></div>
+			<p>Web Design</p>
+			<div id="progress1"></div>
+			<p>HTML</p>
+			<div id="progress2"></div>
+			<p>CSS</p>
+			<div id="progress3"></div>
+
+			</div>
+
+		</div>
+
+		<div class="box-2">
+
+			<div class="intro">
+				<h1>Manoj Adhikari</h1>
+				<hr class="hr" />
+				<div class="clearfix"></div>
+				<p class="intro-p">2132 London (All Airports)-LON,United Kingdom </p>
+				<p>phone +(91)1 234 3455</p>
+				<p>email : demo@gmail.com</p>
+			</div>
+
+			<div class="content-2">
+				<h1 class="head">Experience</h1>
+				<p class="para-1">UI Designer In Lorem Ipsum (2018 - Now)</p>
+				<p class="para-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+
+				<p class="para-1">UI Designer (2013 - 2015)</p>
+				<p class="para-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+
+				<p class="para-1">Grapic Designer(2010)</p>
+				<p class="para-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+			</div>
+
+
+			<div class="content-2">
+				<h1 class="head">Education</h1>
+				<p class="para-1">High school of cbse (2009 - 2010)</p>
+				<p class="para-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+
+				<p class="para-1">Bachelor of Computer Application (2013 - 2015)</p>
+				<p class="para-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+
+
+			</div>
+
+
+		</div>
+
+	</div>
+	<div class="clearfix"></div>
+</div>
+</body>
+
+</html>
