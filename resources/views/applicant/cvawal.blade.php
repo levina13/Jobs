8d9f41715cd40d265b979915333a7c9e7016451f
Dinda Rahma Juwita <dindrahma706@gmail.com>
2023-04-10 18:34:21 +0700

    benerin dikit

--- resources/views/applicant/cvawal.blade.php
+++ resources/views/applicant/cvawal.blade.php
@@ -76,18 +76,4 @@
     <script src="assets/js/lightbox-plus-jquery.js"></script>
 </body>
 @include('layouts.footer')
-<footer>
-    <div class="container py-4">
-        <div class="copyright">
-          &copy; Kelompok 4 <strong> <span>Jobs</span></strong>. All Rights Reserved
-        </div>
-        <div class="credits">
-          <!-- All the links in the footer should remain intact. -->
-          <!-- You can delete the links only if you purchased the pro version. -->
-          <!-- Licensing information: https://bootstrapmade.com/license/ -->
-          <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/ninestars-free-bootstrap-3-theme-for-creative/ -->
-          Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-        </div>
-      </div>
-</footer>
 </html>
